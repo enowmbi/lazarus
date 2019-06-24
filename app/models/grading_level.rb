296609113445dfa1dfc02cef0a1f8fef94739e9b
Enow Mbi <benowmbi@yahoo.com>
2019-06-24 15:15:05 +0100

    adjust scopes to rails 5 style

--- app/models/grading_level.rb
+++ app/models/grading_level.rb
@@ -23,9 +23,9 @@ class GradingLevel < ActiveRecord::Base
   validates_presence_of :credit_points, :if=>:batch_has_gpa
   validates_uniqueness_of :name, :scope => [:batch_id, :is_deleted],:case_sensitive => false 
 
-  default_scope :order => 'min_score desc'
-  named_scope   :default, :conditions => { :batch_id => nil, :is_deleted => false }
-  named_scope   :for_batch, lambda { |b| { :conditions => { :batch_id => b.to_i, :is_deleted => false } } }
+  default_scope {order('min_score desc')}
+  scope :default, lambda{where(:batch_id => nil,  :is_deleted => false) }
+  scope :for_batch, lambda{|b| where(:batch_id => b.to_i, :is_deleted => false ) } 
 
   def inactivate
     update_attribute :is_deleted, true
@@ -47,7 +47,7 @@ class GradingLevel < ActiveRecord::Base
     else
       return true
     end
-  end
+ end
   
   class << self
     def percentage_to_grade(percent_score, batch_id)
