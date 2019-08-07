1cb38f641b608e1921bfaddd5d2c7cb9e2519328
Enow Mbi <benowmbi@yahoo.com>
2019-08-07 16:28:00 +0100

    Modify elective_group

--- app/models/elective_group.rb
+++ app/models/elective_group.rb
@@ -22,7 +22,7 @@ class ElectiveGroup < ApplicationRecord
 
   validates_presence_of :name,:batch_id
 
-  scope :for_batch, lambda { |b| where({:batch_id => b, :is_deleted => false }) }
+  scope :for_batch, lambda { |b| where(:batch_id => b, :is_deleted => false) }
 
   def inactivate
     update_attribute(:is_deleted, true)
