6d61f99a715ab9498b1bc678b3b513693ae391a9
Enow Mbi <benowmbi@yahoo.com>
2019-08-08 12:52:58 +0100

    Modify exam scores controller

--- app/controllers/exam_scores_controller.rb
+++ app/controllers/exam_scores_controller.rb
@@ -17,8 +17,8 @@
 #limitations under the License.
 
 class ExamScoresController < ApplicationController
-  before_filter :login_required
+  before_action :login_required
   in_place_edit_for :exam_score, :score
-  before_filter :protect_other_student_data
+  before_action :protect_other_student_data
   filter_access_to :all
 end
