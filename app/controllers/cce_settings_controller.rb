8daa834e2f08bb20ca7fcce7425a395e5831754d
Enow Mbi <benowmbi@yahoo.com>
2019-08-08 05:13:53 +0100

    Modify cce settings controller

--- app/controllers/cce_settings_controller.rb
+++ app/controllers/cce_settings_controller.rb
@@ -17,7 +17,7 @@
 #limitations under the License.
 
 class CceSettingsController < ApplicationController
-  before_filter :login_required
+  before_action :login_required
   filter_access_to :all
   def index
   end
