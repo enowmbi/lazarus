ca2bea8536b88dc7d43dc3dd09faf214b71396f4
Enow Mbi <benowmbi@yahoo.com>
2019-08-08 05:12:18 +0100

    Modify configuration controller

--- app/controllers/configuration_controller.rb
+++ app/controllers/configuration_controller.rb
@@ -17,33 +17,33 @@
 #limitations under the License.
 
 class ConfigurationController < ApplicationController
-  before_filter :login_required
+  before_action :login_required
   filter_access_to :all
 
   FILE_EXTENSIONS = [".jpg",".jpeg",".png"]#,".gif",".png"]
   FILE_MAXIMUM_SIZE_FOR_FILE=1048576
 
   def settings
-    @config = Configuration.get_multiple_configs_as_hash ['InstitutionName', 'InstitutionAddress', 'InstitutionPhoneNo', \
+    @config = Config.get_multiple_configs_as_hash ['InstitutionName', 'InstitutionAddress', 'InstitutionPhoneNo', \
         'StudentAttendanceType', 'CurrencyType', 'ExamResultType', 'AdmissionNumberAutoIncrement','EmployeeNumberAutoIncrement', \
         'NetworkState','Locale','FinancialYearStartDate','FinancialYearEndDate','EnableNewsCommentModeration','DefaultCountry','TimeZone','FirstTimeLoginEnable']
     @grading_types = Course::GRADINGTYPES
-    @enabled_grading_types = Configuration.get_grading_types
+    @enabled_grading_types = Config.get_grading_types
     @time_zones = TimeZone.all
     @school_detail = SchoolDetail.first || SchoolDetail.new
     @countries=Country.all
     if request.post?
-      Configuration.set_config_values(params[:configuration])
+      Config.set_config_values(params[:configuration])
       session[:language] = nil unless session[:language].nil?
       @school_detail.logo = params[:school_detail][:school_logo] if params[:school_detail].present?
       unless @school_detail.save
-        @config = Configuration.get_multiple_configs_as_hash ['InstitutionName', 'InstitutionAddress', 'InstitutionPhoneNo', \
+        @config = Config.get_multiple_configs_as_hash ['InstitutionName', 'InstitutionAddress', 'InstitutionPhoneNo', \
             'StudentAttendanceType', 'CurrencyType', 'ExamResultType', 'AdmissionNumberAutoIncrement','EmployeeNumberAutoIncrement', \
             'NetworkState','Locale','FinancialYearStartDate','FinancialYearEndDate','EnableNewsCommentModeration','DefaultCountry','TimeZone','FirstTimeLoginEnable']
         return
       end
       @current_user.clear_menu_cache
-      Configuration.clear_school_cache(@current_user)
+      Config.clear_school_cache(@current_user)
       News.new.reload_news_bar
       flash[:notice] = "#{t('flash_msg8')}"
       redirect_to :action => "settings"  and return
