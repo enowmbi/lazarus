ed4b0f6869639c41434f089284a3befd3f6921f6
Enow Mbi <benowmbi@yahoo.com>
2019-08-09 13:00:58 +0100

    scheduled jobs controller

--- app/controllers/scheduled_jobs_controller.rb
+++ app/controllers/scheduled_jobs_controller.rb
@@ -17,9 +17,10 @@
 #limitations under the License.
 
 class ScheduledJobsController < ApplicationController
-  before_filter :login_required
+  before_action :login_required
   filter_access_to :all
   def index
+    #TODO 'use sidekiq"
     @jobs = Delayed::Job.all
     @all_jobs = @jobs.dup
     unless params[:job_object].nil? and params[:job_type].nil?
