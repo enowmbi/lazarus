2e4081b44ccf4588abba09a59f9546dff1504793
Enow Mbi <benowmbi@yahoo.com>
2019-08-09 12:59:41 +0100

    Modify observations controller

--- app/controllers/observations_controller.rb
+++ app/controllers/observations_controller.rb
@@ -17,7 +17,7 @@
 #limitations under the License.
 
 class ObservationsController < ApplicationController
-  before_filter :login_required
+  before_action :login_required
   filter_access_to :all
   def show
     @observation=Observation.find params[:id]
