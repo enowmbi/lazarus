7f719953007529d15f60b178eb2e7c156c8f249c
Enow Mbi <benowmbi@yahoo.com>
2019-08-15 05:19:53 +0100

    Modify rails_helper - restored to previous state after adding require 'factory_bot'

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -4,8 +4,6 @@ SimpleCov.start do
   add_filter '/test/'
   add_filter '/spec/'
 end
-# This file is copied to spec/ when you run 'rails generate rspec:install'
-require 'spec_helper'
 ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../../config/environment', __FILE__)
 # Prevent database truncation if the environment is production
