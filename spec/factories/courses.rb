c19a65a82c6298c1562e169baae6f03f098bdef2
Enow Mbi <benowmbi@yahoo.com>
2019-08-15 03:57:15 +0100

    Create factories directory to hold factories

---
+++ spec/factories/courses.rb
@@ -0,0 +1,10 @@
+FactoryBot.define do
+  factory :course do 
+    course_name "Banking and Finance"
+    code "BF"
+    section_name "2019"
+    is_deleted false
+    grading_type "CCE"
+  end
+end
+
