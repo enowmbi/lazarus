1dda860b62ffb5fa2f36b94db94f8cac7e6b3c83
Enow Mbi <benowmbi@yahoo.com>
2019-08-17 00:49:09 +0100

    Modify user notifier preview

--- spec/mailers/previews/user_notifier_preview.rb
+++ spec/mailers/previews/user_notifier_preview.rb
@@ -3,12 +3,12 @@ class UserNotifierPreview < ActionMailer::Preview
 
   # Preview this email at http://localhost:3000/rails/mailers/user_notifier/forgot_password
    def forgot_password
-     UserNotifierMailer.forgot_password
+     UserNotifierMailer.forgot_password(current_user,"http://localhost:3000")
    end
 
   # Preview this email at http://localhost:3000/rails/mailers/user_notifier/get_domain
    def get_domain
-     UserNotifierMailer.get_domain
+     UserNotifierMailer.get_domain("http://localhost:3000")
    end
 
 end
