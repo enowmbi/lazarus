bb03b54e369451ce1a6873e19d453d91ac234f5a
Enow Mbi <benowmbi@yahoo.com>
2019-09-01 22:05:21 +0100

    Add student categories factory

---
+++ spec/factories/schools.rb
@@ -0,0 +1,8 @@
+FactoryBot.define do 
+  factory :school do 
+    name {"Institute of Technology"}
+    code {"001"}
+  end
+end
+
+
---
+++ spec/factories/student_categories.rb
@@ -0,0 +1,7 @@
+FactoryBot.define do 
+  factory :student_category do 
+    name {"International Students"}
+    is_deleted {false}
+    school
+  end
+end
