8853e260aa61859e91b3035d7331be0d15333571
Enow Mbi <benowmbi@yahoo.com>
2019-09-04 20:10:52 +0100

    Add montly payslip - create factory

---
+++ spec/factories/monthly_payslips.rb
@@ -0,0 +1,15 @@
+FactoryBot.define do 
+  factory :monthly_payslips do 
+    salary_date { Time.now }
+    employee
+    payroll_category
+    amount {"100"}
+    is_approved { true }
+    approver_id {employee.id}
+    is_rejected { false }
+    rejector_id {employee.id}
+    reason {"Monthly payout"}
+    school
+    remark {"This is a remark"}
+  end
+end
