4ea29d81363ef43e5f818a15cb1b88273e1347fd
Enow Mbi <benowmbi@yahoo.com>
2019-09-14 14:48:03 +0100

    Add more users to user factories

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,33 +1,44 @@
 FactoryBot.define do 
-  factory :admin do 
+  factory :user do 
     sequence(:username){|n| "john_doe_#{n}"}
-
     first_name{"John "}
-
     last_name{"doe"}
-
     email{"#{username}@example.com"}
-
-    admin{true}
-
+    admin{false}
     student{false}
-
-    employee {false}
-
+    employee {true}
     password {"jojo"}
-
     salt{"jesus is lord"}
-
-    school
-
+    # school
     parent {false}
-
     is_deleted{false}
   end
 
-  factory :student_user, parent: :admin do 
+  factory :student_user, parent: :user do 
+    sequence(:username){|n| "john_doe_student_#{n}"}
+     email {"#{username}@example.com"}
      student {true}
+     employee {false}
      admin{false}
+     parent{false}
+  end
+
+  factory :admin_user, parent: :user do 
+    sequence(:username){|n| "john_doe_admin_#{n}"}
+     email {"#{username}@example.com"}
+    student {false}
+    employee {false}
+    admin{true}
+    parent{false}
+  end
+
+  factory :parent_user, parent: :user do 
+    sequence(:username){|n| "john_doe_parent_#{n}"}
+     email {"#{username}@example.com"}
+    student {false}
+    employee {false}
+    admin{false}
+    parent{true}
   end
 
 end
