e6391dafd86f67a92f1b9a0de1fd6b24ab08df51
Enow Mbi <benowmbi@yahoo.com>
2019-09-14 13:38:46 +0100

    Modify countries factory - added more other countries

--- spec/factories/countries.rb
+++ spec/factories/countries.rb
@@ -1,5 +1,17 @@
 FactoryBot.define do 
   factory :country do 
-     name {"Cameroon"}
+    name {"Cameroon"}
   end
+  factory :nationality,parent: :country do 
+    name {"USA"}
+  end
+
+  factory :office_country, parent: :country do 
+    name {"Sweden"}
+  end
+
+  factory :home_country, parent: :country do 
+    name {"Switzerland"}
+  end
+
 end
