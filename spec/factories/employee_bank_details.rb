1495642c859a9230df0d4df4864f50a75ad43920
Enow Mbi <benowmbi@yahoo.com>
2019-09-14 15:13:13 +0100

    Added more entries to employee_bank_details

--- spec/factories/employee_bank_details.rb
+++ spec/factories/employee_bank_details.rb
@@ -1,8 +1,8 @@
 FactoryBot.define do 
   factory :employee_bank_details do 
-    employee
+    student_employee
     bank_field
     bank_info {"Atlantic Bank"}
-    school
+    # school
   end
 end
