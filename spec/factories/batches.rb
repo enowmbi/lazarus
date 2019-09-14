112b2eb7751b634a53d45031aeb699577faeaaad
Enow Mbi <benowmbi@yahoo.com>
2019-09-14 12:46:15 +0100

    Modify batch

--- spec/factories/batches.rb
+++ spec/factories/batches.rb
@@ -2,8 +2,8 @@ FactoryBot.define do
   factory :batch  do 
     course 
     name {"Batch"}
-    start_date {2019-01-01}
-    end_date {2019-07-31}
+    start_date {3.months.before}
+    end_date {1.months.after}
     is_active {true}
     is_deleted {true}
     employee
@@ -12,8 +12,8 @@ FactoryBot.define do
 
   factory :batch_1, :parent => :batch do 
     name {"Batch 1"}
-    start_date {2019-03-01}
-    end_date {2019-06-30}
+    start_date {6.months.before}
+    end_date {1.month.before}
     employee_1
   end
 
