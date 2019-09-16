3ff98832fac82ac454db7ca44b3b4aa2a8bcc77c
Enow Mbi <benowmbi@yahoo.com>
2019-09-16 16:41:49 +0100

    Modify school spec - added graceful destroyal line and examples for uniqueness and presence of code

--- spec/models/school_spec.rb
+++ spec/models/school_spec.rb
@@ -15,9 +15,23 @@ RSpec.describe School,type: :model do
     end
 
     it "should ensure the uniqueness of #name" do 
-      {is_expected.to validate_uniqueness_of(:name)}
+      FactoryBot.create(:school,name: "School of Technology")
+      expect{FactoryBot.create(:school,name: "School of Technology")}.to raise_error.with_message(/Name is already taken/)
     end
 
-    it "should ensure presence of #code"
+    it "should ensure presence of #code" do 
+      school = FactoryBot.build(:school,code: nil) 
+      expect(school).not_to be_valid
+    end
+
+    it "should ensure uniqueness of #code" do 
+       FactoryBot.create(:school,code: 'SOT')
+       expect{FactoryBot.create(:school,code:'SOT')}.to raise_error.with_message(/Code is already taken/)
+    end
+  end
+
+  describe "Graceful Destroyal" do 
+    it "should destroy all associated batches"
+    it "should destroy all assciated courses"
   end
 end
