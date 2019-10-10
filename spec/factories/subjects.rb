20b836d82dc0869590c0de21c85fcee2ab990a89
Enow Mbi <benowmbi@yahoo.com>
2019-10-10 16:44:31 +0100

    Update subjects

--- spec/factories/subjects.rb
+++ spec/factories/subjects.rb
@@ -1,13 +1,12 @@
 FactoryBot.define do 
   factory :subject do 
     name {"Accounting" }
-    code {"ACC-201"}
+    sequence(:code){|n| "ACC-201-#{n}"}
     batch
     no_exams { false }
     max_weekly_classes { 3}
     elective_group
     is_deleted {false}
-    school
     language { true }
     credit_hours {70}
     prefer_consecutive { false }
